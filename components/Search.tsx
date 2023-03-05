861c658f304651e64ed0b77c8e3cc6833ed30acf
DucTuLeAnh <56228116+DucTuLeAnh@users.noreply.github.com>
2023-03-05 20:22:03 +0100

    feat: Add search result list without search query backend (#1)
    * feat: Add search result list without search query backend
    
    * feat: move result slicing to query method, since createSearchResultList is just responsible for creating the jsx
    
    * feat: remove unnecessary function calls
    
    * feat: minor cleanups
    
    * feat: minor refactorings
    
    * feat: make component returning function a component itself
    
    * feat: extract search query function to external hook
    
    * feat: remove unused fetcher

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,14 +1,53 @@
-import {FormControl} from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { Note } from "@prisma/client";
+import { ListGroup } from "react-bootstrap";
+import { SearchQueryResult, useSearchResultQuery } from "@/hooks/useSearchResultQuery";
 
-export default function Search() {
-    return (
-        <>
-            <FormControl
-                placeholder="Search notes"
-                autoFocus/>
-            <div className="m-5 text-center text-muted">
-                <p>No recent searches</p>
-            </div>
-        </>
-    );
+function SearchResultList(props: {
+  notes: Omit<Note, "content">[];
+  onNoteSelectChange: (noteId: number) => void;
+}): JSX.Element {
+  return (
+    <ListGroup as="ul" variant="flush">
+      {props.notes.map((note) => (
+        <ListGroup.Item
+          as="li"
+          key={note.id}
+          action
+          className="d-flex gap-2 text-nowrap border-0 text-black"
+          onClick={() => props.onNoteSelectChange(note.id)}
+        >
+          {note.title}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  );
+}
+
+export default function Search(props: {
+  onClose: () => void;
+  onNoteSelectChange: (noteId: number) => void;
+}) {
+  const navigateToNote = (noteId: number) => {
+    props.onNoteSelectChange(noteId);
+    props.onClose();
+  };
+
+  const searchResult: SearchQueryResult = useSearchResultQuery("");
+
+  if (searchResult.error) {
+    //TODO: error handling
+  }
+
+  return (
+    <>
+      <FormControl placeholder="Search notes" autoFocus />
+      <div className="m-5 text-center text-muted">
+        <SearchResultList
+          notes={searchResult.notes}
+          onNoteSelectChange={navigateToNote}
+        />
+      </div>
+    </>
+  );
 }
