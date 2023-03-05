551b07eb5a721e68847a7dc5b5f617f881cbbb29
Duc Tu Le Anh <duc.tu.le.anh@gmail.com>
2023-03-05 19:29:07 +0100

    feat: minor refactorings

--- components/Search.tsx
+++ components/Search.tsx
@@ -6,17 +6,16 @@ import { ListGroup } from "react-bootstrap";
 export const fetcher = (input: RequestInfo, init: RequestInit) =>
   fetch(input, init).then((res) => res.json());
 
-function createSearchResultList(
+function SearchResultList(
   notes: Omit<Note, "content">[],
   onNoteSelectChange: (noteId: number) => void
-): JSX.Element | JSX.Element[] {
-
+): JSX.Element {
   return (
     <ListGroup as="ul" variant="flush">
       {notes.map((note) => (
         <ListGroup.Item
           as="li"
-          key={note.id.toString()}
+          key={note.id}
           action
           className="d-flex gap-2 text-nowrap border-0 text-black"
           onClick={() => onNoteSelectChange(note.id)}
@@ -28,38 +27,41 @@ function createSearchResultList(
   );
 }
 
-//TODO: search Query will be implemented here or extracted to an external service
-function useSearchResultQuery(searchTerm: String): Omit<Note, "content">[] {
-  //TODO: use searchTerm for future search requests
-  const { data: notes } = useSWR<Omit<Note, "content">[]>(
+interface SearchQueryResult {
+  notes: Omit<Note, "content">[];
+  error: Error | undefined;
+  isLoading: boolean;
+}
+
+//TODO: implement proper search query backend
+function useSearchResultQuery(searchTerm: string): SearchQueryResult {
+  const { data, error, isLoading } = useSWR<Omit<Note, "content">[]>(
     "/api/notes",
     fetcher
   );
-
-  var result = notes ?? [];
-
-  if (result.length > 5) {
-    result = result.slice(0, 5);
-  }
-  return result
+  return { notes: data?.slice(0, 5) ?? [], error: error, isLoading: isLoading };
 }
 
 export default function Search(props: {
-  onClosingModal: () => void;
+  onClose: () => void;
   onNoteSelectChange: (noteId: number) => void;
 }) {
   const navigateToNote = (noteId: number) => {
     props.onNoteSelectChange(noteId);
-    props.onClosingModal();
+    props.onClose();
   };
 
   const searchResult = useSearchResultQuery("");
+
+  if (searchResult.error) {
+    //TODO: error handling
+  }
 
   return (
     <>
       <FormControl placeholder="Search notes" autoFocus />
       <div className="m-5 text-center text-muted">
-        {createSearchResultList(searchResult, navigateToNote)}
+        {SearchResultList(searchResult.notes, navigateToNote)}
       </div>
     </>
   );
